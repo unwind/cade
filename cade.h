5a5af3fe9f5023fc1cdfbf90adaea1cf45e0df8c
Emil Brink <emil@obsession.se>
2012-04-09 10:16:51 +0200

    Added your typical garden-variety multiple-inclusion protection.

--- cade.h
+++ cade.h
@@ -1,6 +1,9 @@
 /*
 */
 
+#if !defined CADE_H
+#define	CADE_H
+
 typedef struct DCPU_State	DCPU_State;
 
 void	DCPU_Init(DCPU_State *cpu);
@@ -8,3 +11,5 @@ void	DCPU_Load(DCPU_State *cpu, uint16_t address, const uint16_t *data, size_t l
 void	DCPU_PrintState(const DCPU_State *cpu);
 void	DCPU_Dump(const DCPU_State *cpu, uint16_t start, size_t length);
 void	DCPU_Execute(DCPU_State *cpu, unsigned int num_cycles);
+
+#endif	/* CADE_H */
